7724ba69ba07c28069fcc73c13b639084efd5365
刘志伟 <lzw@spacetech.com.cn>
2020-03-13 17:45:23 +0800

    init

---
+++ src/components/ground.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="game">
+    <ground></ground>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+
+@Component({
+  components: {
+    ground: () => import('@/components/ground.vue')
+  }
+})
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
